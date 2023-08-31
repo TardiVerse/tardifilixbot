95f620d49ad10545f84588d17022adf6a32bfacc
VIGNESH <vigneshmskt@gmail.com>
2023-09-01 01:25:39 +0530

    updated name

--- app/containers/BotHeaderContainer/reducer.js
+++ app/containers/BotHeaderContainer/reducer.js
@@ -8,7 +8,7 @@ import { fromJS } from 'immutable';
 import * as C from './constants';
 
 const initialState = fromJS({
-  botName: 'FiliBot Assessment',
+  botName: 'FiliBot Day Class',
   active: true,
   expanded: false,
 });
