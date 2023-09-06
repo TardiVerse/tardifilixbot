121b34296a3e606e6611ff181e72a2bd2a872b9a
VIGNESH <vigneshmskt@gmail.com>
2023-09-06 21:55:56 +0530

    added state

--- app/BotMind/BotMindFlows/index.js
+++ app/BotMind/BotMindFlows/index.js
@@ -192,6 +192,7 @@ chooseclass: {
 choosestate: {
   botPrompt: 'Choose your <strong>State</strong>   ?',
   type: RTypes.TRANSFORMED_TEXT,
+  input: selectField(['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chattisgarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']),
   answers: [
           { nextId: 'usertype' },
   ],
