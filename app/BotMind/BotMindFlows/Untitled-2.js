70ac41acaf6606fb5577702e7ceda438b0ea30f4
VIGNESH <vigneshmskt@gmail.com>
2023-09-06 17:51:31 +0530

    Added state

---
+++ app/BotMind/BotMindFlows/Untitled-2.js
@@ -0,0 +1,9 @@
+choosestate: {
+  botPrompt: 'Choose your <strong>District</strong> @varName  ?',
+  type: RTypes.TRANSFORMED_TEXT,
+  varName: 'userName',
+  input: tagsField(['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chattisgarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']),
+  answers: [
+          { nextId: 'tfidgenration' },
+  ],
+},
--- app/BotMind/BotMindFlows/index.js
+++ app/BotMind/BotMindFlows/index.js
@@ -182,10 +182,19 @@ const questions = {
     type: RTypes.MEDIA,
     answers: [
       {
-        nextId: 'usertype',
+        nextId: 'choosestate',
       },
     ],
   },
+  choosestate: {
+    botPrompt: 'Choose your <strong>State</strong>   ?',
+    type: RTypes.TRANSFORMED_TEXT,
+    input: tagsField(['Andaman and Nicobar Islands', 'Andhra Pradesh', 'Arunachal Pradesh', 'Assam', 'Bihar', 'Chandigarh', 'Chattisgarh', 'Dadra and Nagar Haveli and Daman and Diu', 'Delhi', 'Goa', 'Gujarat', 'Haryana', 'Himachal Pradesh', 'Jammu and Kashmir', 'Jharkhand', 'Karnataka', 'Kerala', 'Ladakh', 'Lakshadweep', 'Madhya Pradesh', 'Maharashtra', 'Manipur', 'Meghalaya', 'Mizoram', 'Nagaland', 'Odisha', 'Puducherry', 'Punjab', 'Rajasthan', 'Sikkim', 'Tamil Nadu', 'Telangana', 'Tripura', 'Uttar Pradesh', 'Uttarakhand', 'West Bengal']),
+    answers: [
+            { nextId: 'usertype' },
+    ],
+  },
+  
   usertype: {
     botPrompt: 'you are ?',
     type: RTypes.TRANSFORMED_TEXT,
@@ -205,7 +214,6 @@ const questions = {
       },
     ],
   },
-
 
 
 
